967e8808e5432ebf1360079cd768a33d2886b5ce
CSHoggard <christianhoggard@gmail.com>
2020-09-14 15:02:43 +0100

    database update

--- script.R
+++ script.R
@@ -10,9 +10,6 @@ data <- import("https://raw.githubusercontent.com/CSHoggard/-tech-data/master/da
 ### stage one: inspection ###
 
 View(data) ### view data
-class(data) ### check class
-nrow(data) ### number of rows
-ncol(data) ### number of columns
 names(data) ### call column names
 head(data) ### call first few rows
 str(data) ### call data structure
@@ -22,7 +19,9 @@ str(data) ### call data structure
 cols <- c("CODE", "CONTEXT", "COUNTRY", "CONDITION", "ARTEFACT", "SUB_CATEGORY", "RECOVERY_METHOD", "CLASSIFICATION", "BP_ASSOCIATION", "TP_ASSOCIATION", "ABS_DATE_METHOD", "RELAT_DATE_METHOD", "RELAT_DATE_CHRONO", "RAW_MAT", "DORS_BLADE_PROF", "BLADE_DET", "BLADE_CURV", "DORSAL_PATTERN", "BULB_MORPH", "CONUS_FORM", "BUTT_MORPH", "BUTT_PREP_1", "BUTT_PREP_2", "CORE_MORPH", "PLAT_REJUV", "CORE_METHOD", "CORE_DIRECTIONALITY", "CORE_TABLET_REJUV", "CORE_FLAKE_REJUV", "CORE_FRONT_REJUV", "CORE_DIST_REJUV", "CORE_SIDE_REJUV", "ORIENTATION", "BURINATION", "MODEL_DATA") ### call the columns needing converting to factor
 data[cols] <- lapply(data[cols], factor) ### convert above the factors
 
-data_quant <- select(data, CODE, ARTEFACT, CONDITION, SUB_CATEGORY, CLASSIFICATION, WEIGHT, LENGTH, WIDTH, ELONGATION, THICKNESS, PLAT_DEPTH, CORE_LENGTH, CORE_WIDTH, CORE_ELONGATION, TCSA, TCSP, BURINATION)
-data_qual  <- select(data, CODE, ARTEFACT, CONDITION, SUB_CATEGORY, CLASSIFICATION, DORS_BLADE_PROF, BLADE_DET, BLADE_CURV, DORSAL_PATTERN, BULB_MORPH, BUTT_PREP_1, BUTT_PREP_2, CORE_MORPH, PLAT_REJUV, CORE_METHOD, CORE_DIRECTIONALITY, CORE_TABLET_REJUV, CORE_FLAKE_REJUV, CORE_FRONT_REJUV, CORE_DIST_REJUV, CORE_SIDE_REJUV)
+blade_data <- filter(data, ARTEFACT == "Blade")
+core_data <- filter(data, ARTEFACT == "Core")
+bp_data <- filter(data, ARTEFACT == "Backed Point")
+tp_data <- filter(data, ARTEFACT == "Tanged Point")
 
-data_quant_bp <- select_if(data_quant, "ARTEFACT" == Backed Point)
+# stage three: 
